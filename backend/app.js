1facf80e2dac84fe54d44a3134ba4da3ef534e7f
Sandeep Dhakad <165567147+SandeepDhakad01@users.noreply.github.com>
2024-07-13 22:06:28 +0530

    comment manual cors headers

--- backend/app.js
+++ backend/app.js
@@ -24,13 +24,13 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL); // Update this with your frontend URL
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials',true)
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL); // Update this with your frontend URL
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.header('Access-Control-Allow-Credentials',true)
+//     next();
+//   });
 
 app.get('/',(req,res)=>{
     res.status(200)
