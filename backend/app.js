9a8c1883ce8a3e6c02900052c56cda32663fb1ba
Sandeep Dhakad <165567147+SandeepDhakad01@users.noreply.github.com>
2024-07-13 20:42:48 +0530

    manually added cors headers

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL); // Update this with your frontend URL
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials',true)
     next();
   });
 
