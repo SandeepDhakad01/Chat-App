d3ba36fa9f5f9071b33948fa31cc1689e91002f3
SandeepDhakad01 <sandeepdhakad.jay@gmail.com>
2024-07-13 20:28:07 +0530

    manual pass headers

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,16 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 
+
+// Middleware function to add CORS headers
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL); // Update this with your frontend URL
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
+
 app.get('/',(req,res)=>{
     res.status(200)
        .json({
